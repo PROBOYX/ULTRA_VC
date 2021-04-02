b4687024cf87aa94d04129bf5a294a05f74ed5b0
PROBOY X <68194873+PROBOYX@users.noreply.github.com>
2021-04-02 10:27:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from pyrogram import Client as Bot
 import os
 from callsmusic import run
 from config import API_ID, API_HASH, BOT_TOKEN
-from telethon import TelegramClient, Button
 
 bot = Bot(
     ":memory:",
