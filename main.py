db0fcdd63c104d50bfa2217e5b77eb1831745c1c
PROBOY X <68194873+PROBOYX@users.noreply.github.com>
2021-04-02 10:01:37 +0530

    Hmm

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from pyrogram import Client as Bot
-
+import os
+os.system("pip install telethon")
 from callsmusic import run
 from config import API_ID, API_HASH, BOT_TOKEN
-
+from telethon import TelegramClient, Button
 
 bot = Bot(
     ":memory:",
@@ -12,5 +13,12 @@ bot = Bot(
     plugins=dict(root="handlers")
 )
 
+xbot = TelegramClient("PROBOYX", API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+if __name__ == '__main__':
+  xbot.run_until_disconnected()
+
 bot.start()
 run()
+@xbot.on(events.NewMessage(pattern="/owner"))
+async def owner (event):
+  await xbot.send_message(event.chat_id, "THIS IS MY OWNER", buttons=[[Button.url("PROBOYX", "https://t.me/proboyx")]])
